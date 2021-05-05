98d4e617e3cf58aa2a364f6b863f0c2a30ba4f97
Palak2820 <54177848+Palak2820@users.noreply.github.com>
2021-05-05 15:10:13 +0530

    procfile added

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
+const express = require('express');
+const dotenv = require("dotenv");
+
+
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 
 const methodOverride = require('method-override')
 const app = express()
-
+dotenv.config();
 mongoose.connect('mongodb://localhost/blog', {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
 })
+
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
@@ -20,5 +24,6 @@ app.get('/', async (req, res) => {
 })
 
 app.use('/articles', articleRouter)
- 
-app.listen(5000)
+const port = process.env.PORT || '5000';
+
+app.listen(port,() => console.log('server started on port 5000'))
